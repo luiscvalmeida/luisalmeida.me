833ed862a70dab537f007f7ea79ca62f693843fa
Luís Almeida <luis.almeida@mindera.com>
2018-11-14 21:51:10 +0000

    adds nextjs app

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+
+export default () => (
+  <div>
+    <Head>
+      <title>Luís - Frontend Engineer</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <p>yo!</p>
+  </div>
+);
